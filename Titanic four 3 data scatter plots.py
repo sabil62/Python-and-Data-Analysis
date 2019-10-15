7f029573faa88703d7fb6601cd8feed4634782c1
sabil62 <shresthasabil62@gmail.com>
2019-10-15 22:51:13 +0545

    3data scatter plots

---
+++ Titanic four 3 data scatter plots.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset=pd.read_csv('Titanic.csv')
+
+wrangle=dataset[pd.notnull(dataset['Age'])]
+
+wrangle_grouped=wrangle.groupby('Survived')
+
+wrangle['Age'].plot.hist()
+plt.show()
+
+plt.figure()
+survived=wrangle_grouped['Age']
+survived.plot.hist()
+plt.legend()
+plt.show()
+print(wrangle_grouped['Age'].mean())
